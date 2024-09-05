5356da496fccc1c6f62d94b8e268793be8c12a0a
sun <743685264@qq.com>
2024-09-06 04:03:56 +0800

    Updates

---
+++ meta__docs.6b84654b.async.js
@@ -0,0 +1,25 @@
+"use strict";(self.webpackChunksunt=self.webpackChunksunt||[]).push([[904],{75947:function(e,n,a){a.r(n),a.d(n,{texts:function(){return t}});const t=[{value:"\u4F7F\u7528 npm / yarn / pnpm \u5B89\u88C5",paraId:0,tocIndex:1},{value:`npm install @sunt-ui/components
+npm install @sunt-ui/hooks
+`,paraId:1,tocIndex:1},{value:`yarn add @sunt-ui/components
+yarn add @sunt-ui/hooks
+`,paraId:2,tocIndex:1},{value:`pnpm add @sunt-ui/components
+pnpm add @sunt-ui/hooks
+`,paraId:3,tocIndex:1},{value:"\u4F7F\u7528 ",paraId:4,tocIndex:2},{value:"babel-plugin-import ",paraId:4,tocIndex:2},{value:" \u4F18\u5316\u5F15\u5165\u65B9\u5F0F\uFF0C\u5982\u4E0B\uFF1A",paraId:4,tocIndex:2},{value:`import { Button } from '@sunt-ui/components'; // \u4E0E\u4E0A\u8FF0\u793A\u4F8B\u7B49\u4EF7
+
+ReactDOM.render(<Button text="\u666E\u901A\u6309\u94AE" style={{ width: 200 }} />, mountNode);
+`,paraId:5,tocIndex:2},{value:"\u5B89\u88C5 ",paraId:6,tocIndex:2},{value:"babel-plugin-import",paraId:6,tocIndex:2},{value:`npm install babel-plugin-import --save-dev
+yarn add babel-plugin-import --dev
+pnpm add babel-plugin-import --D
+`,paraId:7,tocIndex:2},{value:"\u914D\u7F6E",paraId:8,tocIndex:2},{value:".babelrc",paraId:8,tocIndex:2},{value:" \u6216 ",paraId:8,tocIndex:2},{value:"babel-loader",paraId:8,tocIndex:2},{value:`{
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "@sunt-ui/components",
+        "libraryDirectory": "esm", // default: lib
+        "style": true // or 'css'
+      }
+    ]
+  ]
+}
+`,paraId:9,tocIndex:2}]},63104:function(e,n,a){a.r(n),a.d(n,{texts:function(){return t}});const t=[{value:"@sunt-ui",paraId:0}]}}]);
